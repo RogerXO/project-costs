11cca40faf7fbaa67a7f2d8573383d5d53e4759f
RogerXavier <rogerxavier10@gmail.com>
2022-03-09 20:47:35 -0300

    The most beautiful company and contact pages ever

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,5 +1,12 @@
 function Company() {
-    return <h1>Company</h1>
+    return (
+        <div>
+            <h1>Company</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, debitis quam. Ea quia minima ab cumque deleniti? Provident optio, sequi aspernatur consectetur a delectus excepturi nesciunt, placeat incidunt neque repellat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, debitis quam. Ea quia minima ab cumque deleniti? Provident optio, sequi aspernatur consectetur a delectus excepturi nesciunt, placeat incidunt neque repellat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, debitis quam. Ea quia minima ab cumque deleniti? Provident optio, sequi aspernatur consectetur a delectus excepturi nesciunt, placeat incidunt neque repellat!</p>
+        </div>
+    )
 }
 
 export default Company
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,17 @@
 function Contact() {
-    return <h1>Contact</h1>
+    return (
+        <div>
+            <h1>Our Contacts</h1>
+            <div>
+                <div>
+                    <span><strong>MSN:</strong></span> msn@msn.com
+                </div>
+                <div>
+                    <span><strong>Orkut:</strong></span> orkut@orkut.com
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
